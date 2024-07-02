5800311f6d739919b71b4e06a0ba92a5f3128fd7
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-07-02 18:46:35 +0600

    todoitem design and delete button add

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,12 @@
+import styles from "./todoitem.module.css"
 export default function TodoItem({item}){
-    return <h3>{item}</h3>
+    return <div className={styles.item}>
+        <div className={styles.itemName}>
+            {item}
+            <span className={styles.delete}>
+                <button>x</button>
+            </span>
+        </div>
+        <hr className={styles.line} />
+    </div>
 }
